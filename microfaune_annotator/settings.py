62f79d7c868df993dcda7fb5e74503cf55a9650d
Thomas Creuillenet <thomas.creuillenet@capgemini.com>
2020-10-27 22:18:01 +0100

    Remove commented code

--- microfaune_annotator/settings.py
+++ microfaune_annotator/settings.py
@@ -76,15 +76,6 @@ WSGI_APPLICATION = 'microfaune_annotator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-""" 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-} 
-"""
-
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
