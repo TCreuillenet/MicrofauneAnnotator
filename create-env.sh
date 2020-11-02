b2cd2b5e34fe7ccf19f9758ee25e761d24e5e0d0
Thomas Creuillenet <thomas.creuillenet@capgemini.com>
2020-11-02 21:35:21 +0100

    Deploy environment with RDS database

--- create-env.sh
+++ create-env.sh
@@ -10,20 +10,12 @@ else
     echo "====== Will create environment : $ENVIRONMENT"
 fi
 
-if [ "${DEPLOY_RDS}" == "true" ]; then
-    echo "====== Will create RDS database"
-fi
-
 # create application if not created yet
 eb init $APPLICATION_NAME -p python-3.7
 
 INSTANCE_OPTIONS="--region eu-west-3 --single -i t2.micro"
 DB_OPTIONS="-db.engine postgres -db.i db.t2.micro -db.user ebroot -db.pass microfaune -db.size 5"
 
-if [ "${DEPLOY_RDS}" == "true" ]; then
-    eb create $ENVIRONMENT $INSTANCE_OPTIONS $DB_OPTIONS
-else
-    eb create $ENVIRONMENT $INSTANCE_OPTIONS
-fi
+eb create $ENVIRONMENT $INSTANCE_OPTIONS $DB_OPTIONS
 
 echo "====== DONE"
